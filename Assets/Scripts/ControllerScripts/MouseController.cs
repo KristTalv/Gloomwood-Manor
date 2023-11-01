3dbd36de41aaaf7ede457e7850ee578bfb494fc4
KristTalv <krista.talvipuro@edu.turkuamk.fi>
2023-11-01 15:24:16 +0200

    Changes to cursor sprites, modifications to mouse controller script, edited the Frist scene

--- Assets/Scripts/ControllerScripts/MouseController.cs
+++ Assets/Scripts/ControllerScripts/MouseController.cs
@@ -6,8 +6,11 @@ public class MouseController : MonoBehaviour
 {
     // String
     public string mouseScriptCursorName;
+    // Bools
+    private bool red;
     // Sprites
     [SerializeField] private Texture2D[] cursor;
+    [SerializeField] private Texture2D cursor_active;
     //[SerializeField] private Texture2D def_cursor;
 
     public void SetCursor(string name)
@@ -19,6 +22,24 @@ public class MouseController : MonoBehaviour
             {
                 Cursor.SetCursor(cursor[i], Vector3.zero, CursorMode.ForceSoftware);
             }
+        }
+    }
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if(hit.transform.tag == "quest" || hit.transform.tag == "Puzzle" || hit.transform.tag == "Item")
+            {
+                Cursor.SetCursor(cursor_active, Vector3.zero, CursorMode.ForceSoftware);
+            }
+            else
+            {
+            
+                Cursor.SetCursor(default, Vector3.zero, CursorMode.ForceSoftware);
+            }
+
         }
     }
 
